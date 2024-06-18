cc3e9c8354f5b2306cc2aaf16f38514705edaa29
Mahad <mahad852@gmail.com>
2024-06-18 20:34:10 +0500

    training for 20 epochs using 5e-5 lr and 20 epochs

--- finetune_llama.py
+++ finetune_llama.py
@@ -21,7 +21,7 @@ pred_len = 64
 ecg_dataset = ECG_MIT(context_len=context_len, pred_len=pred_len, data_path="/home/mali2/datasets/ecg/MIT-BIH.npz")
 
 batch_size = 64
-total_samples = 1000000
+total_samples = 200000
 
 
 indices = random.sample(range(len(ecg_dataset)), total_samples)
